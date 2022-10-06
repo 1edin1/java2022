7c66b3a44bae3c02dd8e5c94b969f5685f460e26
Edin Avdic <Edin.Avdic@htlstp.at>
2022-10-06 11:57:58 +0200

    README

---
+++ src/at/ac/htlstp/sj222a/einf/HalloWelt.java
@@ -0,0 +1,7 @@
+package at.ac.htlstp.sj222a.einf;
+
+public class HalloWelt {
+    public static void main(String[] args) {
+        System.out.println("HalloWelt");
+    }
+}
---
+++ src/at/ac/htlstp/sj222a/einf/SMUE_UEBUNG.java
@@ -0,0 +1,9 @@
+package at.ac.htlstp.sj222a.einf;
+
+public class SMUE_UEBUNG {
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");
+
+    }
+}
+
